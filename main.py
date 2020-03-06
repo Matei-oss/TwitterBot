e2e6e63b1e6dbad336ef524b7d55db8304653a25
Matei-oss <andrei_mateiu1996@live.com>
2020-03-06 17:45:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,62 @@
+#Twitter Bot
+
+#requires Twitter API
+
+# use tweepy.org 
+
+import tweepy
+import time
+
+# consumer_key,consumer_secret, access_toker, access_token_secret are default values from the Twitter API website (don't share them)
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.home_timeline()
+for tweet in public_tweets:
+    print(tweet.text) #prints tweets from the home timeline 
+
+user = api.me() # prints information about user
+
+print(user.name)
+print(user.followers_count) # shows the number of followers 
+
+
+def limit_handler(cursor):
+  try:
+    while True:
+      yield cursor.next()
+  except tweepy.RateLimitError:
+    time.sleep(1000)
+
+
+# BOT THAT FOLLOWS BACK
+
+for follower in limit_handler(tweepy.Cursor(api.followers).items):
+  if follower.name == 'follower-name':
+    follow.follow()
+    break
+
+  print(follower.name)
+
+
+#NARCISSIST BOT
+
+search_string = 'bentley'
+
+numberOfTweets = 2 
+
+for tweet in tweepy.Cursor(api.search, search_string).items(numberOfTweets):
+  try:
+    tweet.favourite()
+    print('That tweet was likeable')
+  except tweepy.TweepyError as e:
+    print(e.reason)
+  except StopIteration:
+    break
+
+
+
+
